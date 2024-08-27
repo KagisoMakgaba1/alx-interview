f13750984701f6d677e87c05127ea0e100b7e5af
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-08-27 01:06:43 -0700

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,93 +1,44 @@
 #!/usr/bin/python3
-
-"""Solves the N-Queens challenge."""
-
-
-def n_queens(n: int):
-    """
-    Solve the N-Queens challenge.
-
-    Args:
-        n (int): The size of the chessboard.
-
-    Returns:
-        The chessboard with the Queens placed in non-attacking positions.
-    """
-
-    def is_non_attack(board: list, row: int, col: int) -> bool:
-        """
-        Check whether a slot is non-attacking when a Queen is placed.
-
-        Args:
-            board (list): The board to check.
-            row (int): The current row on the board.
-            col (int): The column of the board.
-
-        Returns:
-            bool: True if the position leads to a non-attacking placement,
-            else False
-        """
-        for i in range(row):
-            if (
-                board[i] == col  # check for vertical possible attacks
-                or board[i] - i == col - row
-                or board[i] + i == col + row
-            ):
-                return False
-
-        return True
-
-    def insert_queen(board: list, row: int) -> None:
-        """
-        Handle the placement of Queens in a non-attacking fashion.
-
-        Args:
-            board (list): The board to insert into (temporal).
-            row (int): The current row in the board.
-        """
-        if row == n:  # base case
-            chessboard.append([[i, board[i]] for i in range(n)])
-            return
-
-        for col in range(n):
-            if is_non_attack(board, row, col):
-                board[row] = col
-                insert_queen(board, row + 1)
-                board[row] = 0
-
-    chessboard = []
-    board = [0] * n
-    insert_queen(board, 0)
-
-    return chessboard
+""" N queens """
+import sys
 
 
-def print_non_attack_combinations(chessboard) -> None:
-    """
-    Print all the non-attacking combinations of N-Queens.
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    Args:
-        chessboard: The list of all non-attacking combinations.
-    """
-    for row in chessboard:
-        print(row)
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+n = int(sys.argv[1])
 
 
-if __name__ == "__main__":
-    """Entry point"""
-    import sys
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
 
-    if not sys.argv[1].isdigit():
-        print("N must be a number")
-        sys.exit(1)
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
-    N = int(sys.argv[1])
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
 
-    print_non_attack_combinations(n_queens(N))
+solve(n)
